1e5968abcac589b56abe560389020983385d8a24
philip.jaeyoon.jung <im182cm@gmail.com>
2015-09-15 07:23:09 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JjyAws::Application.config.secret_key_base = '7ee2e881b6265bc19153566b5c0be6299abf7d642100bf5def244100ebc91c686ea23fc6dd9738fc4329e0fa95f30c7b2dfffc6855ee0215411346afa34bb46d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JjyAws::Application.config.session_store :cookie_store, key: '_jjy_aws_session'
